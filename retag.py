944a7d9c13ba8d330a19a43b2c9dc8e35e0cc8cb
Igor Serebryany <igor47@moomers.org>
2012-10-17 02:38:29 -0700

    initial support for ogg files via mutagen

--- retag.py
+++ retag.py
@@ -2,6 +2,7 @@
 
 import os
 import eyeD3
+import mutagen, mutagen.oggvorbis
 
 for dirpath, dirnames, filenames in os.walk('/media/music'):
    try:
@@ -9,25 +10,42 @@ for dirpath, dirnames, filenames in os.walk('/media/music'):
    except:
       continue
 
-   mp3s = filter(lambda f: f.endswith('mp3'), filenames)
-   for mp3 in mp3s:
+   media_files = [f for f in filenames if (f.endswith('ogg') or f.endswith('mp3'))]
+   for mfile in media_files:
       try:
-         track, title = mp3.split(' ', 1)
+         track, title = mfile.split(' ', 1)
          track = int(track)
       except:
          continue
 
-      path = os.path.join(dirpath, mp3)
+      path = os.path.join(dirpath, mfile)
 
-      try:
-         t = eyeD3.Tag()
-         t.link(path)
-         ctrack, cmax = t.getTrackNum()
-      except:
-         continue
+      if mfile.endswith('mp3'):
+         try:
+            t = eyeD3.Tag()
+            t.link(path)
+            ctrack, cmax = t.getTrackNum()
+         except:
+            continue
 
-      if track != ctrack:
-         t.setVersion(eyeD3.tag.ID3_DEFAULT_VERSION)
-         t.setTrackNum((track, len(mp3s)))
-         t.update()
-         print "%s => %d on %s" % (ctrack, t.getTrackNum()[0], path)
+         if track != ctrack:
+            t.setVersion(eyeD3.tag.ID3_DEFAULT_VERSION)
+            t.setTrackNum((track, len(media_files)))
+            t.update()
+
+      elif mfile.endswith('ogg'):
+         try:
+            t = mutagen.oggvorbis.open(path)
+         except:
+            continue
+         else:
+            try:
+               ctrack = t['tracknumber'][0]
+            except:
+               ctrack = None
+
+         if track != ctrack:
+            t['tracknumber'] = "%02d" % track
+            t.save()
+
+      print "%s => %d on %s" % (ctrack, track, path)
