1fa54f83b83d4a07a294b48e0a0373d0a3bafad5
Igor Serebryany <igor47@moomers.org>
2016-07-14 01:19:45 -0500

    [retag] make it work for all kinds of files
    this script was just used to re-tag about 10k files which were properly named
    but didn't have correct tags. if the tag was missing, we set it from the
    filename. if computed tag and the existing tag disagreed, we logged it, but
    didn't change anything.

--- retag.py
+++ retag.py
@@ -1,51 +1,120 @@
 #!/usr/bin/python
 
-import os
-import eyeD3
-import mutagen, mutagen.oggvorbis
+import os, os.path
+import mutagen
 
-for dirpath, dirnames, filenames in os.walk('/media/music'):
-   try:
-      artist, album = dirpath.split('/media/music/')[1].split('/')
-   except:
-      continue
+# root music directory. this is where all music is stored. we assume
+# that every directory under this root corresponds to an artist name
+root_music_dir = '/media/music'
 
-   media_files = [f for f in filenames if (f.endswith('ogg') or f.endswith('mp3'))]
-   for mfile in media_files:
-      try:
-         track, title = mfile.split(' ', 1)
-         track = int(track)
-      except:
+# these are the fields we'd like to set on tracks
+fields = ['album', 'artist', 'title', 'tracknumber']
+
+def walk_media(root):
+   for dirpath, dirnames, filenames in os.walk(root):
+      dir_without_root = dirpath.split(root_music_dir)[1]
+      parts = dir_without_root.split('/')[1:]
+      if len(parts) == 1:
+         artist = parts[0]
+         album = None
+      elif len(parts) == 2:
+         artist = parts[0]
+         album = parts[1]
+      else:
          continue
 
-      path = os.path.join(dirpath, mfile)
-
-      if mfile.endswith('mp3'):
+      for name in filenames:
+         # derive track and title
+         without_extension = os.path.splitext(name)[0]
          try:
-            t = eyeD3.Tag()
-            t.link(path)
-            ctrack, cmax = t.getTrackNum()
+            track, title = without_extension.split(' ', 1)
          except:
-            continue
+            title = without_extension
+            track = None
 
-         if track != ctrack:
-            t.setVersion(eyeD3.tag.ID3_DEFAULT_VERSION)
-            t.setTrackNum((track, len(media_files)))
-            t.update()
-
-      elif mfile.endswith('ogg'):
-         try:
-            t = mutagen.oggvorbis.Open(path)
-         except:
-            continue
-         else:
+         # make sure track is an integer (even though we keep it as a string)
+         if track is not None:
             try:
-               ctrack = t['tracknumber'][0]
+               int(track)
             except:
-               ctrack = None
+               title = without_extension
+               track = None
 
-         if track != ctrack:
-            t['tracknumber'] = "%02d" % track
-            t.save()
+         # is this a media file? find out by creating a mutagen FileType object
+         path = os.path.join(dirpath, name)
+         try:
+            ft = mutagen.File(path, easy = True)
+         except Exception, e:
+            print "%-30s: invalid file, i guess? %s" % (path, e)
+            continue
 
-      print "%s => %d on %s" % (ctrack, track, path)
+         # this was not a media file that mutagen recognized 
+         if ft is None:
+            print "%-30s: skipped (notmedia)" % path
+            continue
+
+         if type(ft) == mutagen.aiff.AIFF:
+            print "%-30s: skipped (aiff unsupported)" % path
+            continue
+
+         if type(ft) == mutagen.aac.AAC:
+            print "%-30s: skipped (aac unsupported)" % path
+            continue
+
+         # if we don't have any tags, lets add some
+         if ft.tags is None:
+            ft.add_tags()
+
+         # now lets set the fields that are missing
+         computed_fields = {
+               'artist': artist,
+               'album': album,
+               'title': title,
+               'tracknumber': track,
+               }
+
+         updated_fields = {}
+         disagreed_fields = {}
+
+         for field, value in computed_fields.items():
+            # skip if we don't even know what the right value should be
+            if value is None:
+               continue
+
+            # is the value already set? if so we don't update it, but we do log disagreements
+            try:
+               existing_value = ft.tags[field]
+
+               # sometimes, it's a list
+               try:
+                  existing_value = existing_value[0]
+               except IndexError:
+                  pass
+
+               if field == 'tracknumber':
+                  try:
+                     existing_value = int(existing_value)
+                  except ValueError:
+                     pass
+
+                  if existing_value != int(value):
+                     disagreed_fields[field] = (existing_value, value)
+               elif existing_value != value:
+                  disagreed_fields[field] = (existing_value, value)
+
+            # value is not already set, so lets use the value we've got
+            except KeyError:
+               ft.tags[field] = [value]
+               updated_fields[field] = value
+
+         if len(updated_fields) > 0:
+            print "%-30s: updated: %s" % (path, updated_fields)
+            ft.save()
+         else:
+            print "%-30s: up2date!" % path
+
+         if len(disagreed_fields) > 0:
+            print "%-30s: disagreed: %s" % (path, disagreed_fields)
+
+if __name__ == "__main__":
+   walk_media('/media/music')
